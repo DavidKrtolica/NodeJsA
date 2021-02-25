1a74ac16d05556f7934a7d2f66b1ab76aa3de376
David <david.krtolica8@gmail.com>
2021-02-25 08:55:29 +0100

    Changed the way of declaring the days and months data as const outside the scope of the methods.

--- 4.TimeServer/app.js
+++ 4.TimeServer/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+//MADE A LIST THAT CONTAINS ALL DAYS OF THE WEEK, THEN FETCHING IT THROUGH THE INDEX
+//DECLARING IT OUTSIDE THE SCOPE OF THE GET METHOD, SO IT LOADS THIS DATA ONLY ONCE THE SERVER STARTS
+//AND NOT EVERY TIME THE GET METHOD IS CALLED AT THAT ENDPOINT
+  const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//SAME AS THE DAYS LIST, BUT WITH MONTHS - FETCHING CORRECT MONTH THROUGH THE INDEX
+//DECLARING IT OUTSIDE THE SCOPE OF THE GET METHOD, SO IT LOADS THIS DATA ONLY ONCE THE SERVER STARTS
+//AND NOT EVERY TIME THE GET METHOD IS CALLED AT THAT ENDPOINT
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 //HOMEPAGE FOR THE TIME API
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to my Time API !</h1>To access the current time, write \"/time\".<br>To access the current day of the month,"
@@ -12,14 +22,10 @@ app.get("/time", (req, res) => {
 });
 
 app.get("/day", (req, res) => {
-    //MADE A LIST THAT CONTAINS ALL DAYS OF THE WEEK, THEN FETCHING IT THROUGH THE INDEX
-    const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     res.send({currentDateInMonth: new Date().getDate(), currentDayOfWeek: dayOfWeek[new Date().getDay()]});
 });
 
 app.get("/month", (req, res) => {
-    //SAME AS THE DAYS LIST, BUT WITH MONTHS - FETCHING CORRECT MONTH THROUGH THE INDEX
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     res.send({currentMonth: months[new Date().getMonth()]});
 });
 
